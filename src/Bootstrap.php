578c4e2f0c9a8dd348217313fb114f7feff7f3fb
ecrona <ecrona@gmail.com>
2016-07-07 18:53:40 +0200

    Normalized bootstrap file

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -1,16 +1,12 @@
 <?php
-
 namespace Etmp;
-
 use Etmp\Foundation\Router;
 use Etmp\Render\Renderer;
-
 abstract class Bootstrap {
     public static function start()
     {
         global $argv;
         $router = new Router($argv);
-
         $controller = $router->getController();
         $view       = $controller->dispatch();
         
