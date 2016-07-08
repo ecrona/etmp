b6325e2b1b14ab2b6f84587f708dd0b549062a92
ecrona <ecrona@gmail.com>
2016-07-08 20:51:56 +0200

    Added further functionality to the MySQL adapter, wrote guidelines for adapter usage in the configuration dist file

--- src/Storage/Adapter/MySQL/Storage.php
+++ src/Storage/Adapter/MySQL/Storage.php
@@ -7,6 +7,7 @@ use Etmp\Storage\Adapter;
 use Exception;
 use DateTime;
 use DateInterval;
+use PDO;
 
 class Storage implements Adapter {
     private $dir;
@@ -35,36 +36,72 @@ class Storage implements Adapter {
         $this->db->exec('
             CREATE TABLE IF NOT EXISTS `logs` (
                 value TEXT NOT NULL,
-                date TIMESTAMP NOT NULL,
-                level VARCHAR 50 NOT NULL
+                date DATETIME NOT NULL,
+                level VARCHAR(50) NOT NULL
             );
         ');
 
         $this->db->exec('
             CREATE TABLE IF NOT EXISTS `adress` (
                 value TEXT NOT NULL,
-                date TIMESTAMP NOT NULL
+                date DATETIME NOT NULL,
+                identifier TINYINT(1) NOT NULL,
+                PRIMARY KEY (identifier)
             );
+
+            INSERT IGNORE INTO `adress` (`value`, `identifier`)
+            VALUES ("", 1)
         ');
     }
 
     public function read(string $table): string
     {
-        return '';
+        $sth = $this->db->prepare('
+            SELECT `value` FROM `' . $table . '`
+            LIMIT 1
+        ');
+
+        $sth->execute();
+
+        return $sth->fetchColumn();
     }
 
     public function append(string $table, $date, $value, array $metadata = array())
     {
+        $columns = array_merge(['value'], array_keys($metadata));
 
+        $sth = $this->db->prepare('
+            INSERT INTO `' . $table . '` (' . implode(', ', $columns) . ')
+            VALUES (:' . implode(', :', $columns) . ')
+        ');
+
+        $sth->execute(array_merge([
+            'value' => $value
+        ], $metadata));
     }
 
     public function insert(string $table, $date, $value)
     {
+        $sth = $this->db->prepare('
+            UPDATE `' . $table . '`
+            SET `value` = :value, `date`  = :date
+        ');
 
+        $sth->execute([
+            'value' => $value,
+            'date' => $date->format('Y-m-d H:i:s'),
+        ]);
     }
     
     public function clean(string $table, int $days = 7)
     {
+        $sth = $this->db->prepare('
+            DELETE FROM `' . $table . '`
+            WHERE DATEDIFF(NOW(), date) > :days
+        ');
 
+        $sth->execute([
+            'days' => $days
+        ]);
     }
 }
